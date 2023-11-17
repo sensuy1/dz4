b8a4050c3be9d59f455a278ceabc02f0237087a0
sensuy1 <sultanbejsenbekov@gmail.com>
2023-11-17 22:43:52 +0600

    dz4

---
+++ app.js
@@ -0,0 +1,28 @@
+// var array =  ['name', 'John', 'lastname', 'Black', 'age', '23']
+// var object = {}
+//
+// for(var i = 0; i < array.length - 1; i++){
+//     if(array[i] !== 'John' && array[i] !== 'Black'){
+//         object[array[i]] = array[i + 1]
+//     }
+// }
+// console.log(object)
+
+
+// function average(arr) {
+//     return arr.reduce((sum, number) => sum + number, 0) / arr.length
+// }
+//
+// var numbers = [7, 4, 10, 8]
+// var result = average(numbers)
+// console.log(result)
+
+
+
+// var getDataType = (value) =>  {
+//     console.log(typeof value)
+// }
+//
+// getDataType(false)
+// getDataType('привет')
+// getDataType(69)
